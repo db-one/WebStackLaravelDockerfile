54598568db2091df0422d00e2b94992418366ffb
unknown <kljxn@qq.com>
2020-02-09 19:12:42 +0800

    最终版本？

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,16 @@ LABEL "Mail":"kljxnn@gmail.com"\
 ENV RUN_USER daemon
 ENV RUN_GROUP daemon
 ENV INSTALL_DIR /opt/navi
-ENV DB_HOST 172.18.0.2
+ENV DB_HOST 172.18.0.4
 ENV DB_PORT 3306
 ENV DB_DATABASE webstack
 ENV DB_USERNAME root
 ENV DB_PASSWORD root
 ENV LOGIN_COPTCHA true
+ENV ENTRYPOINT_CHOICE new-server
 
-ARG DOWNLOAD_URL=https://github.com/iyzyi/WebStack-Laravel/archive/v1.3.1.tar.gz
+
+ARG DOWNLOAD_URL=http://file.iyzyi.com/package/WebStack-Laravel/WebStack-Laravel-1.3.1.tar.gz
 EXPOSE 8000
 
 COPY entrypoint.sh /entrypoint.sh
@@ -30,7 +32,13 @@ RUN curl -L --silent  ${DOWNLOAD_URL} | tar -xz --strip-components=1 -C "${INSTA
     && cp .env.example .env \
     && chown -R ${RUN_USER}:${RUN_GROUP} ${INSTALL_DIR}\
     && chown -R ${RUN_USER}:${RUN_GROUP} /entrypoint.sh
+RUN chmod 777 /entrypoint.sh
+
+#RUN : \
+#    && /bin/echo -e '#!/bin/sh\n' >> /run.exe \
+#    && bin/echo "/entrypoint.sh ${ENTRYPOINT_CHOICE}" >> /run.exe \
+#    && chmod +x /run.exe
+#CMD ["/run.exe"]
+CMD /entrypoint.sh ${ENTRYPOINT_CHOICE}
 
 WORKDIR ${INSTALL_DIR}
-CMD ["/entrypoint.sh", "serve"]
-ENTRYPOINT ["/sbin/tini", "--"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,70 @@
 #!/bin/sh
 
-cd ${INSTALL_DIR}
-sed -i -e "s/DB_HOST=.*/DB_HOST=${DB_HOST}/; \
-            s/DB_PORT=.*/DB_PORT=${DB_PORT}/; \
-            s/DB_DATABASE=.*/DB_DATABASE=${DB_DATABASE}/; \
-            s/DB_USERNAME=.*/DB_USERNAME=${DB_USERNAME}/; \
-            s/DB_PASSWORD=.*/DB_PASSWORD=${DB_PASSWORD}/" .env 
-sed -i "/login-captcha/{n;s/'enable.*/'enable' => ${LOGIN_COPTCHA}/}"  config/admin.php
+function init() {
+    cd ${INSTALL_DIR}
+    sed -i -e "s/DB_HOST=.*/DB_HOST=${DB_HOST}/; \
+                s/DB_PORT=.*/DB_PORT=${DB_PORT}/; \
+                s/DB_DATABASE=.*/DB_DATABASE=${DB_DATABASE}/; \
+                s/DB_USERNAME=.*/DB_USERNAME=${DB_USERNAME}/; \
+                s/DB_PASSWORD=.*/DB_PASSWORD=${DB_PASSWORD}/" .env
+    sed -i "/login-captcha/{n;s/'enable.*/'enable' => ${LOGIN_COPTCHA}/}"  config/admin.php
+    echo "成功初始化配置文件"
+}
 
+function info() {
+    echo -e "\n"
+    echo -e "Usage: $0  serve|new-server|key|refresh|listen\n"
+    echo "There are four commands by order:"
+    echo "1. php artisan key:generate"
+    echo "2. php artisan migrate:refresh"
+    echo "3. php artisan serve --host=0.0.0.0 --port=8000"
+    echo "4. php artisan webstack:clean"
+    echo -e "\nHere is the relation between the options and the commands:"
+    echo "serve:        1,3"
+    echo "new-server:   1,2,3"
+    echo "key:          1"
+    echo "refresh:      2"
+    echo "listen:       3"
+    echo "clean:        4"
+}
 
 ##php artisan key:generate
 if [ $# -eq 1 ];then
     if [ $1 == 'serve' ];then
+	if [ ! -e "/first" ]; then
+            init
+	    touch "/first"
+	    echo "没有检测到/first的存在，成功初始化配置文件"
+	fi
+        php artisan key:generate
         php artisan serve --host=0.0.0.0 --port=8000
     elif [ $1 == 'new-server' ];then
+	if [ ! -e "/first" ]; then
+            init
+	fi
         php artisan key:generate
-        result=1
-        while [ $result -ne 0 ];do
-            php artisan migrate:refresh --seed
-            result=$?
-            sleep 3
-        done
+	if [ ! -e "/first" ]; then
+            result=1
+            while [ $result -ne 0 ];do
+                php artisan migrate:refresh --seed
+                result=$?
+                sleep 3
+            done
+	    touch "/first"
+	    echo "没有检测到/first的存在，成功初始化配置文件和数据库"
+        fi
         php artisan serve --host=0.0.0.0 --port=8000
-    elif [ $1 == 'regresh' ];then
-        php artisan migrate:refresh --seed
-    elif [ $1 == 'generate' ];then
+    elif [ $1 == 'key' ];then
         php artisan key:generate
+    elif [ $1 == 'refresh' ];then
+        php artisan migrate:refresh --seed
+    elif [ $1 == 'listen' ];then
+        php artisan serve --host=0.0.0.0 --port=8000
+    elif [ $1 == 'clean' ];then
+        php artisan webstack:clean
+    else 
+        info
     fi
 else
-    echo "Usage: $0  serve|regresh|generate"
+    info
 fi
